Sonarr/Sonarr

New: Download client option for redownloading failed releases from Interactive Search
Closes #5580

src/NzbDrone.Core/Download/FailedDownloadService.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Download.TrackedDownloads;
 using NzbDrone.Core.History;
 using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.Download
 {
@@ -128,6 +130,7 @@ namespace NzbDrone.Core.Download
         private void PublishDownloadFailedEvent(List<EpisodeHistory> historyItems, string message, TrackedDownload trackedDownload = null, bool skipRedownload = false)
         {
             var historyItem = historyItems.First();
+            Enum.TryParse(historyItem.Data.GetValueOrDefault(EpisodeHistory.RELEASE_SOURCE, ReleaseSourceType.Unknown.ToString()), out ReleaseSourceType releaseSource);
 
             var downloadFailedEvent = new DownloadFailedEvent
             {
@@ -141,7 +144,8 @@ namespace NzbDrone.Core.Download
                 Data = historyItem.Data,
                 TrackedDownload = trackedDownload,
                 Languages = historyItem.Languages,
-                SkipRedownload = skipRedownload
+                SkipRedownload = skipRedownload,
+                ReleaseSource = releaseSource
             };
 
             _eventAggregator.PublishEvent(downloadFailedEvent);

src/NzbDrone.Core/Download/RedownloadFailedDownloadService.cs
@@ -5,6 +5,7 @@ using NzbDrone.Core.IndexerSearch;
 using NzbDrone.Core.Messaging;
 using NzbDrone.Core.Messaging.Commands;
 using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Tv;
 
 namespace NzbDrone.Core.Download
@@ -39,6 +40,12 @@ namespace NzbDrone.Core.Download
             if (!_configService.AutoRedownloadFailed)
             {
                 _logger.Debug("Auto redownloading failed episodes is disabled");
+                return;
+            }
+
+            if (message.ReleaseSource == ReleaseSourceType.InteractiveSearch && !_configService.AutoRedownloadFailedFromInteractiveSearch)
+            {
+                _logger.Debug("Auto redownloading failed episodes from interactive search is disabled");
                 return;
             }
 

sharinganthief/Lidarr

(cherry picked from commit 87e0a7983a437a4d166aa8b9c9eaf78ea5431969)

src/NzbDrone.Core/Download/FailedDownloadService.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Download.TrackedDownloads;
 using NzbDrone.Core.History;
 using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.Download
 {
@@ -117,6 +119,7 @@ namespace NzbDrone.Core.Download
         private void PublishDownloadFailedEvent(List<EntityHistory> historyItems, string message, TrackedDownload trackedDownload = null, bool skipRedownload = false)
         {
             var historyItem = historyItems.Last();
+            Enum.TryParse(historyItem.Data.GetValueOrDefault(EntityHistory.RELEASE_SOURCE, ReleaseSourceType.Unknown.ToString()), out ReleaseSourceType releaseSource);
 
             var downloadFailedEvent = new DownloadFailedEvent
             {
@@ -129,7 +132,8 @@ namespace NzbDrone.Core.Download
                 Message = message,
                 Data = historyItem.Data,
                 TrackedDownload = trackedDownload,
-                SkipRedownload = skipRedownload
+                SkipRedownload = skipRedownload,
+                ReleaseSource = releaseSource
             };
 
             _eventAggregator.PublishEvent(downloadFailedEvent);

src/NzbDrone.Core/Download/RedownloadFailedDownloadService.cs
@@ -5,6 +5,7 @@ using NzbDrone.Core.Messaging;
 using NzbDrone.Core.Messaging.Commands;
 using NzbDrone.Core.Messaging.Events;
 using NzbDrone.Core.Music;
+using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.Download
 {
@@ -38,6 +39,12 @@ namespace NzbDrone.Core.Download
             if (!_configService.AutoRedownloadFailed)
             {
                 _logger.Debug("Auto redownloading failed albums is disabled");
+                return;
+            }
+
+            if (message.ReleaseSource == ReleaseSourceType.InteractiveSearch && !_configService.AutoRedownloadFailedFromInteractiveSearch)
+            {
+                _logger.Debug("Auto redownloading failed albumbs from interactive search is disabled");
                 return;
             }
 